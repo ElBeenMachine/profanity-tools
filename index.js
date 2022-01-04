44aee5634248ccf05aa94c88ba4b67d6c76897e7
Ollie Beenham <beenhamow@outlook.com>
2022-01-04 15:29:05 +0000

    Update index.js

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ class Profanity {
         
         substring.forEach(element => {
             let wordMatches = [];
+
             filter.forEach(word => {
                 const regex = new RegExp(word, "ig");
                 let matches = element.match(regex);
@@ -15,6 +16,7 @@ class Profanity {
                     wordMatches.push(matches[0]);
                 }
             });
+
             if(wordMatches.length) {
                 matchedWords.push({
                     word: element,
@@ -34,42 +36,46 @@ class Profanity {
     censor(text) {
         let substring = text.split(" ");    
         let outputString = "";
+        let outputWord = "";
 
         substring.forEach(element => {
             let wordMatches = [];
             let isProfanic = false;
             filter.forEach(word => {
-                let outputWord = "";
                 const regex = new RegExp(word, "ig");
                 let matches = element.match(regex);
                 if(matches) {
                     wordMatches.push(matches[0]);
-                    for(let i = 0; i <= element.length - 1; i++) {
-                        if(i == 0 || i == element.length - 1) {
-                            outputWord += element[i];
-                        } else {
-                            outputWord += "*"
-                        }
-                    }
-                    outputString += `${outputWord} `;
                     isProfanic = true
                 }
             });
-            if(!isProfanic) outputString += `${element} `;
+            
+            if(!isProfanic) {
+                outputString += `${element} `;
+            } else {
+                outputWord = "";
+                for(let i = 0; i <= element.length - 1; i++) {
+                    if(i == 0 || i == element.length - 1) {
+                        outputWord += element[i];
+                    } else {
+                        outputWord += "*"
+                    }
+                }
+                outputString += `${outputWord} `;
+            }
         });
         
         return outputString;
     }
 
-    check(text) {
+    hasProfanity(text) {
         const data = this.identify(text);
         return data.isProfanic ? true : false;
     }
 }
 
 module.exports = {
-    hasProfanity: new Profanity().check,
+    hasProfanity: new Profanity().hasProfanity,
     identify: new Profanity().identify,
     censor: new Profanity().censor
 }
-
